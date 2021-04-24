bb60f508e4718d4c43f2b956758cd4fa64fb2283
tamioTech <62820237+tamioTech@users.noreply.github.com>
2021-04-24 13:12:17 -0700

    levelLoader control start Menu and Splash screen

--- GlitchGarden/Assets/Scripts/LevelLoaderGG.cs
+++ GlitchGarden/Assets/Scripts/LevelLoaderGG.cs
@@ -6,21 +6,63 @@ using UnityEngine.SceneManagement;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class LevelLoaderGG : MonoBehaviour
 {
 
     [SerializeField] AudioClip quitGameButtonSound;
     [SerializeField] AudioClip startButtonSound;
+    [SerializeField] [Range(0,1)] float startButtonVolume = 1.0f;
+    [SerializeField] [Range(0, 1)] float quitButtonVolume = 1.0f;
+    [SerializeField] int scenePosition;
+    [SerializeField] int timeToWait = 3;
+
+    void Start ()
+    {
+        scenePosition = SceneManager.GetActiveScene().buildIndex;
+        if (scenePosition == 0)
+        {
+            StartCoroutine(LoadNextScene());
+        }
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        SceneManager.LoadScene(scenePosition + 1);
+    }
 
     public void StartButtonPushed()
     {
         Debug.Log("Great jooooorb!");
-        AudioSource.PlayClipAtPoint(startButtonSound, Camera.main.transform.position);
+        AudioSource.PlayClipAtPoint(startButtonSound, Camera.main.transform.position, startButtonVolume);
     }
 
     public void QuitButtonPressed()
     {
         Debug.Log("Where you goin' boi!? Mama raised no quitter!");
-        AudioSource.PlayClipAtPoint(quitGameButtonSound, Camera.main.transform.position);
+        AudioSource.PlayClipAtPoint(quitGameButtonSound, Camera.main.transform.position, quitButtonVolume);
     }
+
 }
