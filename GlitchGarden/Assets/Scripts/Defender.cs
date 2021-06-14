a3680e71585950e7b2336d6a5cccba3bb6d01b48
tamioTech <62820237+tamioTech@users.noreply.github.com>
2021-06-14 00:04:21 -0700

    working game! now add some levels!

--- GlitchGarden/Assets/Scripts/Defender.cs
+++ GlitchGarden/Assets/Scripts/Defender.cs
@@ -5,11 +5,35 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
 
-    [SerializeField] int starCost = 100;
+    [SerializeField] int starCost;
+    int cactusCost = 150;
+    int starTrophyCost = 75;
+    int gnomeCost = 375;
+    int tombStoneCost = 225;
 
     public int GetStarCost()
     {
         return starCost;
+    }
+
+    public int GetCacutsCost()
+    {
+        return cactusCost;
+    }
+
+    public int GetStarTropyCost()
+    {
+        return starTrophyCost;
+    }
+
+    public int GetGnomeCost()
+    {
+        return gnomeCost;
+    }
+
+    public int GetTombStoneCost()
+    {
+        return tombStoneCost;
     }
 
     public void AddStars(int amount)
